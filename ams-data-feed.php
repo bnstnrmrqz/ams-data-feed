658e320cdf3bffeeadca811891b049fe75cd8f9c
Ben Steiner Marquez <8839574+bnstnrmrqz@users.noreply.github.com>
2025-02-17 13:12:52 -0600

    Update ams-data-feed.php

--- ams-data-feed.php
+++ ams-data-feed.php
@@ -1,13 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
  * @link              https://github.com/bnstnrmrqz
  * @since             1.0.0
  * @package           Ams_Data_Feed
@@ -26,7 +19,8 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if(!defined('WPINC'))
+{
 	die;
 }
 
@@ -35,14 +29,15 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'AMS_DATA_FEED_VERSION', '1.1.0' );
+define('AMS_DATA_FEED_VERSION', '1.1.0');
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-ams-data-feed-activator.php
  */
-function activate_ams_data_feed() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-ams-data-feed-activator.php';
+function activate_ams_data_feed()
+{
+	require_once plugin_dir_path(__FILE__).'includes/class-ams-data-feed-activator.php';
 	Ams_Data_Feed_Activator::activate();
 }
 
@@ -50,24 +45,25 @@ function activate_ams_data_feed() {
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-ams-data-feed-deactivator.php
  */
-function deactivate_ams_data_feed() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-ams-data-feed-deactivator.php';
+function deactivate_ams_data_feed()
+{
+	require_once plugin_dir_path(__FILE__).'includes/class-ams-data-feed-deactivator.php';
 	Ams_Data_Feed_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_ams_data_feed' );
-register_deactivation_hook( __FILE__, 'deactivate_ams_data_feed' );
+register_activation_hook(__FILE__, 'activate_ams_data_feed');
+register_deactivation_hook(__FILE__, 'deactivate_ams_data_feed');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-ams-data-feed.php';
+require plugin_dir_path(__FILE__).'includes/class-ams-data-feed.php';
 
 /**
  * Include the shortcodes functionality file.
  */
-include_once plugin_dir_path( __FILE__ ) . 'includes/ams-shortcode.php';
+include_once plugin_dir_path(__FILE__).'includes/ams-shortcode.php';
 
 /**
  * Begins execution of the plugin.
@@ -78,10 +74,9 @@ include_once plugin_dir_path( __FILE__ ) . 'includes/ams-shortcode.php';
  *
  * @since    1.0.0
  */
-function run_ams_data_feed() {
-
+function run_ams_data_feed()
+{
 	$plugin = new Ams_Data_Feed();
 	$plugin->run();
-
 }
 run_ams_data_feed();
