6402bccde03f281f77f52c756369e546477aedec
Ben Steiner Marquez <8839574+bnstnrmrqz@users.noreply.github.com>
2025-02-17 13:21:47 -0600

    Update class-ams-data-feed-i18n.php

--- includes/class-ams-data-feed-i18n.php
+++ includes/class-ams-data-feed-i18n.php
@@ -24,24 +24,19 @@
  * @subpackage Ams_Data_Feed/includes
  * @author     Ben Steiner Marquez <bnstnrmrqz@gmail.com>
  */
-class Ams_Data_Feed_i18n {
-
-
+class Ams_Data_Feed_i18n
+{
 	/**
 	 * Load the plugin text domain for translation.
 	 *
 	 * @since    1.0.0
 	 */
-	public function load_plugin_textdomain() {
-
+	public function load_plugin_textdomain()
+    {
 		load_plugin_textdomain(
 			'ams-data-feed',
 			false,
-			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+			dirname(dirname(plugin_basename(__FILE__))).'/languages/'
 		);
-
 	}
-
-
-
 }
