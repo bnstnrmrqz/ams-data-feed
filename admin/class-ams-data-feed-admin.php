bc174b5300702fb3be7295b7ac7e333c701df692
Ben Steiner Marquez <8839574+bnstnrmrqz@users.noreply.github.com>
2025-02-17 13:15:34 -0600

    Update class-ams-data-feed-admin.php

--- admin/class-ams-data-feed-admin.php
+++ admin/class-ams-data-feed-admin.php
@@ -20,8 +20,8 @@
  * @subpackage Ams_Data_Feed/admin
  * @author     Ben Steiner Marquez <bnstnrmrqz@gmail.com>
  */
-class Ams_Data_Feed_Admin {
-
+class Ams_Data_Feed_Admin
+{
 	/**
 	 * The ID of this plugin.
 	 *
@@ -47,11 +47,10 @@ class Ams_Data_Feed_Admin {
 	 * @param      string    $plugin_name       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
-
+	public function __construct($plugin_name, $version)
+	{
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
-
 	}
 
 	/**
@@ -59,8 +58,8 @@ class Ams_Data_Feed_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_styles() {
-
+	public function enqueue_styles()
+	{
 		/**
 		 * This function is provided for demonstration purposes only.
 		 *
@@ -72,9 +71,7 @@ class Ams_Data_Feed_Admin {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/ams-data-feed-admin.css', array(), $this->version, 'all' );
-
+		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__).'css/ams-data-feed-admin.css', array(), $this->version, 'all' );
 	}
 
 	/**
@@ -82,8 +79,8 @@ class Ams_Data_Feed_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_scripts() {
-
+	public function enqueue_scripts()
+	{
 		/**
 		 * This function is provided for demonstration purposes only.
 		 *
@@ -95,9 +92,6 @@ class Ams_Data_Feed_Admin {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ams-data-feed-admin.js', array( 'jquery' ), $this->version, false );
-
+		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__).'js/ams-data-feed-admin.js', array('jquery'), $this->version, false);
 	}
-
 }
